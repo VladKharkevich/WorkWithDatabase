2b2eaf130f48b46ae68503d150bbd0ab23b4cfbd
VladKharkevich <kharkevichvlad@gmail.com>
2020-11-30 14:26:57 +0300

    Updated files that are not in src folder:)

--- migrator/dump.sql
+++ migrator/dump.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS classroom DEFAULT CHARACTER SET utf8;
 USE classroom;
 CREATE TABLE IF NOT EXISTS `room`(
-	`room_id` INT(10) NOT NULL AUTO_INCREMENT,
+	`room_id` INT(10) NOT NULL,
 	`name` VARCHAR(20) NOT NULL,
 	PRIMARY KEY (`room_id`) 	
 ) ENGINE=innodb DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS `student`(
-	`student_id` INT(10) NOT NULL AUTO_INCREMENT,
+	`student_id` INT(10) NOT NULL,
 	`name` VARCHAR(100) NOT NULL,
 	`birthday` DATETIME NOT NULL,
 	`sex` CHAR(1) NOT NULL,
@@ -14,5 +14,6 @@ CREATE TABLE IF NOT EXISTS `student`(
 	PRIMARY KEY (`student_id`),
 	CONSTRAINT `student_room_id_fk`
 		FOREIGN KEY (`room_id`) REFERENCES `room`(`room_id`)
+		ON UPDATE CASCADE
 		ON DELETE CASCADE
 ) ENGINE=innodb DEFAULT CHARSET=utf8;
